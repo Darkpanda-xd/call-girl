b6db134727ce6d6f9152f038478ba5ce7dafb986
partha <roypartha407@gmail.com>
2023-12-25 00:29:27 +0530

    fixed issue changes in index.html

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import './app.css'
 import Navbar from './Navbar/Navbar'
 import Home from './Home/Home'
-import About from './About/about'
+
 import Gallery from './Gallery/Gallery'
 import FixButton from './components/fixedButton/button'
 import Banner from './banner/banner'
 import Contact from './Contact/Contact'
+import About from './About/About'
 
 
 const App = () => {
